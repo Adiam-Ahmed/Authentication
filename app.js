e139d3b9a1ab7696c18610379a0bf15d1d184443
Adiam Ahmed <adu.adu911@gmail.com>
2024-01-11 00:28:20 -0500

    used bcrypt hash

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const express = require("express");//importing the express module, which is a we
 const bodyParser = require("body-parser");//body-parser module parse incoming request in a middleware before your handlers.
 const ejs = require("ejs");// ejs is a templating engine that allows you to embed JavaScript code directly within your HTML files
 const mongoose= require("mongoose")//mongoose is an Object Data Modeling (ODM) library for MongoDB and Node.js
-var md5 = require('md5');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const PORT = process.env.PORT || 3000;
+//var md5 = require('md5');/// is basically used to hash passwords to used , we do md5(....)
 
 ////initializes an instance of the Express application.it configure routes, set up middleware, and define web server behaviour.
 const app = express();
@@ -16,7 +19,8 @@ app.use(bodyParser.urlencoded({
 }));
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/userDB');
+mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/userDB');
+
 
 // Set up a new userSchema Database which will be Js object with two field 
 //I have updated it from plain JavaScript object to mongoose.Schema as its a class and this way we can defining more complex and structured schemas.
@@ -24,10 +28,6 @@ const userSchema= new mongoose.Schema({
     email: String,
     password:String
 });
-
-
-
-
 
 
 // Set up a new user module 
@@ -47,52 +47,47 @@ app.get("/login",function(req,res){
 
 // we have a form that will target the register route,
 // to catch it, we can add our post req  inside our callback 
-app.post("/register", async function(req, res) {
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
-    });
-
+app.post("/register", async (req, res) => {
+    // Extract username and password from the request body
     try {
-        // Use await to wait for the promise to resolve
+        const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
+        const newUser = new User({
+            email: req.body.username,
+            password: hashedPassword
+        });
+
         await newUser.save();
         res.render("secrets");
     } catch (error) {
-        res.redirect("/register"); // Redirect to the registration page in case of an error
+        res.redirect("/register");
     }
 });
 
-app.post("/login", async function(req, res) {
-    // Extract username and password from the request body
+app.post("/login", async (req, res) => {
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
     try {
-        // Attempt to find a user in the database from the user model based on the provided email
+          // Attempt to find a user in the database from the user model based on the provided email
         const foundUser = await User.findOne({ email: username });
 
         if (foundUser) {
-            // If a user with the provided email is found, check if the password matches
-            if (foundUser.password === password) {
-                // If the password matches, render the "secrets" view
+            const passwordMatch = await bcrypt.compare(password, foundUser.password);
+
+            if (passwordMatch) {
                 res.render("secrets");
             } else {
-                // If the password doesn't match, send a response 
                 res.send("Incorrect password");
             }
         } else {
-            // If no user is found with the provided email, send a response
             res.send("Email not found");
         }
     } catch (err) {
-        // Catch any errors that occur during the database operation and send an "Error" response
         res.send("Error");
     }
 });
 
 
-app.listen(3000, function(){
-    console.log("Server started on port 3000")
-})
-
-
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
